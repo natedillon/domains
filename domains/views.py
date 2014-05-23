cb3f9e17212a273d4871dfb7836aa4ca9e9a2377
Nate Dillon <natedillon@gmail.com>
2014-05-23 16:55:59 -0500

    Reconfigured the domain context
    Added the name key.

--- domains/views.py
+++ domains/views.py
@@ -3,8 +3,16 @@ import pythonwhois
 
 def home(request):
 
+    # Initialize the context
     context = {}
 
-    context['domain'] = pythonwhois.get_whois('google.com')
+    # Set the domain name
+    name = 'natedillon.com'
+
+    # Create the domain context
+    context['domain'] = {
+        'name': name,
+        'whois': pythonwhois.get_whois(name),
+    }
 
     return render(request, 'domains/index.html', context)
