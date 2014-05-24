4d781cc5adbf6e9eb2663539617370249f872497
Nate Dillon <natedillon@gmail.com>
2014-05-24 10:17:53 -0500

    Created a form that submits a query

--- domains/views.py
+++ domains/views.py
@@ -1,7 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 import pythonwhois
 
-def home(request):
+
+def home(request, **kwargs):
 
     # Initialize the context
     context = {}
